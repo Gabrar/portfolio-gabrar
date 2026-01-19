3ab4c5c0fb884c3600b3016e1ea512202f730ff0
Gabrar <gabriel2025diasss@gmail.com>
2026-01-18 22:45:45 -0300

    Ajuste no tamanho do card em telas maiores

--- src/components/CardProjetos.jsx
+++ src/components/CardProjetos.jsx
@@ -1,19 +1,22 @@
-import Button from "./Button"
+import { ChevronDown } from "lucide-react"
 
 function Card({ imagem, title, onClick }) {
-    
+
 
     return (
-        <div className="flex flex-col h-50 w-35 bg-white/7 rounded-lg border border-white/40 animation-top opacity-0" style={{animationDelay: '0.1s'}}>
-            <img src={imagem} alt="" className="h-15 w-auto rounded-md m-2" />
+        <div className="flex flex-col h-50 w-35 md:h-70 md:w-45 lg:h-75 lg:w-50 bg-white/7 rounded-lg border border-white/40 animation-top opacity-0" style={{ animationDelay: '0.1s' }}>
+            <img src={imagem} alt="" className="h-15 md:h-20 lg:h-25 w-auto rounded-md m-2" />
 
             <h1 className="px-2">{title}</h1>
 
+
             <div className="grid grid-cols justify-center mt-auto mb-2">
-                <button 
-                onClick={onClick}
-                className='p-3 lg:p-4 bg-black/30 backdrop-blur-md rounded-bl-md rounded-tr-md cursor-pointer hover:bg-black/50 hover:scale-110 transition delay-100 ease-in-out'
-                       
+
+                <ChevronDown className="m-auto animate-bounce" style={{animationDuration: "2s"}}/>
+                <button
+                    onClick={onClick}
+                    className='p-3 lg:p-4 bg-black/30 backdrop-blur-md rounded-bl-md rounded-tr-md cursor-pointer hover:bg-black/50 hover:scale-110 transition delay-100 ease-in-out'
+
                 >
                     Detalhes
                 </button>
