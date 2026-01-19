c3424396aec3e2a4b806cb27aa7043628d297819
Gabrar <gabriel2025diasss@gmail.com>
2026-01-19 10:56:01 -0300

    ...

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom"
 
-function Button({ text, page, animation }) {
+function Button({ text, page, animation, opacity }) {
 
     const navigate = useNavigate()
 
     return (
-        <button className={`p-3 lg:p-4 bg-black/30 backdrop-blur-md rounded-bl-md rounded-tr-md cursor-pointer hover:bg-black/50 hover:scale-110 transition delay-100 ease-in-out ${animation} opacity-0`}
+        <button className={`p-3 lg:p-4 bg-black/30 backdrop-blur-md rounded-bl-md rounded-tr-md cursor-pointer hover:bg-black/50 hover:scale-110 transition delay-100 ease-in-out ${animation} opacity-${opacity}`}
             onClick={() => navigate(`${page}`)}>
             {text}
         </button>
